5d97694382353959957ed014d167d569eb4a6a7b
JosueHenriquez <josueguinea@gmail.com>
2025-09-04 22:43:19 -0600

    Intento deploy14

--- src/main/java/IntegracionBackFront/backfront/BackfrontApplication.java
+++ src/main/java/IntegracionBackFront/backfront/BackfrontApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "IntegracionBackFront.backfront")
 public class BackfrontApplication {
 
     public static void main(String[] args) {
