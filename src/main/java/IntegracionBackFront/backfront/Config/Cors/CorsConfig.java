65dbcce8888881250925ddf939036db977fb6875
JosueHenriquez <josueguinea@gmail.com>
2025-09-13 19:14:56 -0600

    Incorporacion de localhost con http y https

--- src/main/java/IntegracionBackFront/backfront/Config/Cors/CorsConfig.java
+++ src/main/java/IntegracionBackFront/backfront/Config/Cors/CorsConfig.java
@@ -21,6 +21,7 @@ public class CorsConfig {
         // Configuración esencial
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost");
+        config.addAllowedOrigin("https://localhost");
         config.addAllowedOrigin("http://localhost:3000"); // React
         config.addAllowedOrigin("http://localhost:8080"); // Vue
         config.addAllowedOrigin("http://localhost:4200"); // Angular
